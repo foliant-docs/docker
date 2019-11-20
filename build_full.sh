ca36ca3d57cd0cf1ca7d09fa6371977a1c6711b1
Artemy Lomov <lomov@restream.rt.ru>
2019-11-20 13:51:19 +0300

    Do not use cache during the full image build.

--- build_full.sh
+++ build_full.sh
@@ -12,7 +12,7 @@ echo "Building and pushing the full image for Foliant ${FOLIANT_VERSION}"
 
 docker pull foliant/foliant:pandoc
 docker login
-docker build -t foliant/foliant:full -t foliant/foliant:${FOLIANT_VERSION}-full -f Dockerfile-full ./ 2>&1 | tee ./build.log
+docker build --no-cache -t foliant/foliant:full -t foliant/foliant:${FOLIANT_VERSION}-full -f Dockerfile-full ./ 2>&1 | tee ./build.log
 docker push foliant/foliant:full
 docker push foliant/foliant:${FOLIANT_VERSION}-full
 docker logout
