b488e5619eb201db1e7b32617eee4937a7c436b7
Timur Osmanov <54434686+TOsmanov@users.noreply.github.com>
2026-02-25 11:59:21 +0300

    update: for foliant 1.0.14 (#22)
    * update: for foliant 1.0.14
    
    * update: dockerfiles and scripts
    
    * update: dockerfiles and add TODO
    
    * add: TODO
    
    * fix: mssql install
    
    * update: dockerfiles and use ubuntu 20.04 python 3.9
    
    * revert: foliant branch
    
    * update: build scripts
    
    * add: push images
    
    * remove: unnecessary code
    
    * Update Dockerfile-full
    
    * Update Dockerfile-full
    
    * add: foliant-all-publish.yml
    
    * add: clean runner

--- build_latest.sh
+++ build_latest.sh
@@ -1,15 +1,35 @@
 #!/bin/bash -e
 
+os_type=`uname`
+
+if [ "${os_type}" == 'Darwin' ] ; then
+    grep_option='E'
+else
+    grep_option='P'
+fi
+
+if [ ! $1 ] ; then
+    # TODO: switch add-partial-copy to master
+    FOLIANT_VERSION=`curl https://raw.githubusercontent.com/foliant-docs/foliant/add-partial-copy/foliant/__init__.py | grep -${grep_option}o "\d+\.\d+\.\d+"`
+else
+    FOLIANT_VERSION=$1
+fi
 
 echo "Building and pushing the latest image"
 
+BASE_DOCKER_IMAGE=foliant/foliant:slim
 DOCKER_IMAGE_TAG=foliant/foliant
+BASE_DOCKER_IMAGE_NUMBERED_TAG=foliant/foliant:${FOLIANT_VERSION}-slim
+DOCKER_IMAGE_NUMBERED_TAG=foliant/foliant:${FOLIANT_VERSION}
 GITHUB_REGISTRY_URL=ghcr.io
 ORGANIZATION=foliant-docs
 
-docker pull $GITHUB_REGISTRY_URL/$ORGANIZATION/foliant/foliant:slim
+docker pull $GITHUB_REGISTRY_URL/$ORGANIZATION/$BASE_DOCKER_IMAGE
+docker pull $GITHUB_REGISTRY_URL/$ORGANIZATION/$BASE_DOCKER_IMAGE_NUMBERED_TAG
 docker login $GITHUB_REGISTRY_URL
-docker build --no-cache -t $DOCKER_IMAGE_TAG ./ 2>&1 | tee ./build.log
-docker tag $DOCKER_IMAGE_TAG $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_TAG
-docker push $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_TAG
+docker build --no-cache -t $DOCKER_IMAGE_TAG -t $DOCKER_IMAGE_NUMBERED_TAG -f Dockerfile ./ 2>&1 | tee ./build.log
+# docker tag $DOCKER_IMAGE_TAG $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_TAG
+# docker push $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_TAG
+docker tag $DOCKER_IMAGE_NUMBERED_TAG $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_NUMBERED_TAG
+docker push $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_NUMBERED_TAG
 docker logout $GITHUB_REGISTRY_URL
--- build_slim.sh
+++ build_slim.sh
@@ -9,7 +9,8 @@ else
 fi
 
 if [ ! $1 ] ; then
-    FOLIANT_VERSION=`curl https://raw.githubusercontent.com/foliant-docs/foliant/master/foliant/__init__.py | grep -${grep_option}o "\d+\.\d+\.\d+"`
+    # TODO: switch add-partial-copy to master
+    FOLIANT_VERSION=`curl https://raw.githubusercontent.com/foliant-docs/foliant/add-partial-copy/foliant/__init__.py | grep -${grep_option}o "\d+\.\d+\.\d+"`
 else
     FOLIANT_VERSION=$1
 fi
@@ -24,8 +25,8 @@ ORGANIZATION=foliant-docs
 docker pull ubuntu:focal
 docker login $GITHUB_REGISTRY_URL
 docker build --no-cache --build-arg VERSION=${FOLIANT_VERSION} -t $DOCKER_IMAGE_TAG -t $DOCKER_IMAGE_NUMBERED_TAG -f Dockerfile-slim ./ 2>&1 | tee ./build.log
-docker tag $DOCKER_IMAGE_TAG $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_TAG
-docker push $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_TAG
+# docker tag $DOCKER_IMAGE_TAG $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_TAG
+# docker push $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_TAG
 docker tag $DOCKER_IMAGE_NUMBERED_TAG $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_NUMBERED_TAG
 docker push $GITHUB_REGISTRY_URL/$ORGANIZATION/$DOCKER_IMAGE_NUMBERED_TAG
 docker logout $GITHUB_REGISTRY_URL
